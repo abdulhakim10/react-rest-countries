b7f0563ec37606d46b0b33b0a156d59db7a24221
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-09-26 08:34:21 +0600

    event handler with parameter added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Country.css'
 const Country = (props) => {
 
-   const {name, population, region, area, flags} = props.country;
+   const {name, population, region, area, flags, cca3} = props.country;
+//    event handler with parameter
+   const marked = (cca3) =>{
+    console.log('marked country', cca3)
+   }
     return (
         <div className='Country'>
             <h2>Country: {name.common}</h2>
@@ -10,6 +14,8 @@ const Country = (props) => {
             <p>Population: {population}</p>
             <p>Region: {region}</p>
             <p><small>Area: {area}</small></p>
+            <p>Code: {cca3}</p>
+            <button onClick={() => marked(cca3)}>Marked</button>
         </div>
     );
 };
